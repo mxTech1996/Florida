2044d8cab63cb208bf4cfef1158ae17488351f85
juliotwo <juliotwo1996@gmail.com>
2025-04-08 11:39:11 -0600

    Fix

--- src/data/index.js
+++ src/data/index.js
@@ -40,3 +40,244 @@ export const footerData = [
     label: 'Terms',
   },
 ];
+
+export const dataSite = {
+  name: 'Florida',
+  url: 'https://comercializadoraflorida.lat/',
+  telephone: '5558269829',
+  email: 'admflorida@emailsecure.lat',
+  address:
+    'PASEO DE LAS AMERICAS 31, PISO 1 INTERIOR B,  LOMAS VERDES 3A SECCION, NAUCALPAN DE JUAREZ, ESTADO DE MEXICO, C.P. 53125',
+  description:
+    'We specialize in delivering expert solutions to enhance your projects and accelerate innovation. Our team of experienced scientists and engineers provides tailored advice, analysis, and strategic insights across various industries, including biotechnology, environmental science, engineering, and information technology.',
+  subtitle:
+    'Empowering Innovation Through Expert Scientific and Technical Guidance',
+  image_hero:
+    'https://cms-webserver-statics.s3.amazonaws.com/media/websites/flo_1.jpg',
+  image_hero2:
+    'https://cms-webserver-statics.s3.amazonaws.com/media/websites/flo_2.jpg',
+  iconImage:
+    'https://cms-webserver-statics.s3.amazonaws.com/media/websites/logo-no-background_s1y1EY3.png',
+  info: [
+    {
+      title: 'Mission',
+      description:
+        'Our mission is to provide unparalleled scientific and technical expertise that empowers our clients to achieve their goals efficiently and effectively. We are committed to fostering innovation, ensuring high-quality outcomes, and navigating complex challenges with precision and integrity.',
+    },
+    {
+      title: 'Values',
+      description:
+        'Excellence, We strive for the highest standards in every project, ensuring precise and reliable results, Integrity, Our work is driven by honesty, transparency, and ethical principles, Innovation, We encourage creative problem-solving and use cutting-edge techniques to provide forward-thinking solutions, Collaboration, We believe in strong partnerships with our clients, built on trust and open communication.',
+    },
+    {
+      title: 'Vision',
+      description:
+        'To be a global leader in scientific and technical consulting, driving groundbreaking solutions that shape a sustainable and innovative future for industries and communities worldwide',
+    },
+  ],
+  services: [
+    {
+      title: 'Research Project Development Consulting',
+      description:
+        'Assistance in the design, planning, and execution of scientific and technical studies, providing recommendations and evidence-based strategies.',
+      image:
+        'https://cms-webserver-statics.s3.amazonaws.com/media/services/flo4.jpg',
+    },
+    {
+      title: 'Data Analysis and Statistical Modeling',
+      description:
+        'Services for data processing, advanced statistical analysis, and modeling to interpret results from studies or experiments.',
+      image:
+        'https://cms-webserver-statics.s3.amazonaws.com/media/services/flo_5_.jpg',
+    },
+    {
+      title: 'Scientific Writing and Review',
+      description:
+        'Help with writing, editing, and reviewing scientific documents, such as journal articles, technical reports, and conference presentations.',
+      image:
+        'https://cms-webserver-statics.s3.amazonaws.com/media/services/flo_6.jpg',
+    },
+    {
+      title: 'Technological Solutions Development',
+      description:
+        'Design and implementation of specialized tools and software to solve technical problems in industries like biotechnology, engineering, or environmental sciences.',
+      image:
+        'https://cms-webserver-statics.s3.amazonaws.com/media/services/flo_7_.jpg',
+    },
+    {
+      title: 'Training and Workshops',
+      description:
+        'Training programs on research techniques, statistical methodologies, specialized software use, and best practices in scientific research.',
+      image:
+        'https://cms-webserver-statics.s3.amazonaws.com/media/services/flo_8.jpg',
+    },
+  ],
+  references: [
+    {
+      name: 'Emily Rodriguez',
+      description:
+        "I hired this consulting service for a complex research project, and I couldn't be happier with the results. Their team provided invaluable insights and helped me refine my methodology.",
+      rating: 5,
+      image: null,
+    },
+    {
+      name: 'Dr. Mark Samper',
+      description:
+        'The data analysis services exceeded my expectations,They delivered a thorough statistical report that was easy to understand and implement.',
+      rating: 5,
+      image: null,
+    },
+    {
+      name: 'Sarah Lim',
+      description:
+        'I took one of their training workshops on statistical modeling, and it was fantastic! ,The instructor was knowledgeable and engaging, and I left with practical skills that I could apply immediately to my research.',
+      rating: 5,
+      image: null,
+    },
+    {
+      name: 'James Tyson',
+      description:
+        'The consultation package I purchased was well worth the investment,The experts provided actionable advice that helped streamline our project,Their support made the process much smoother',
+      rating: 5,
+      image: null,
+    },
+    {
+      name: 'Lindia Forest',
+      description:
+        'I was impressed with their customized research reports,They tailored the findings specifically to our needs and presented the information in a clear, concise manner. It helped us make informed decisions moving forward.',
+      rating: 5,
+      image: null,
+    },
+  ],
+  products: [
+    {
+      id: 477,
+      name: 'Data Visualization Dashboards',
+      category: null,
+      description:
+        'Data Visualization Dashboards are interactive platforms that present complex data sets in a clear, visual, and easy-to-understand format,These dashboards are essential tools for businesses and organizations that need to monitor, analyze, and present data-driven insights efficiently.',
+      price: '899.99',
+      stock: 100,
+      content:
+        'Interactive Interface, Real-Time Data Updates, Customizable Layouts, Multi-Source Data Integration, Data Filtering and Segmentation, User-Friendly Design,They are designed with user experience in mind, often featuring drag-and-drop interfaces, easy navigation.',
+      product_type: 'NORMAL',
+      image:
+        'https://cms-webserver-statics.s3.amazonaws.com/media/products/flo_10.jpg',
+      image2: null,
+      image3: null,
+    },
+    {
+      id: 479,
+      name: 'Software Tools for Data Analysis',
+      category: null,
+      description:
+        'Software Tools for Data Analysis are specialized applications designed to help users collect, process, and analyze large volumes of data to derive insights, make predictions, and support decision-making.',
+      price: '799.99',
+      stock: 100,
+      content:
+        'Data Collection and Integration, Data Cleaning and Preparation, Advanced Analytical Capabilities, Most tools offer intuitive user interfaces with drag-and-drop functionalities, making them accessible even to users with limited technical expertise,Tools feature robust data visualization capabilities to create charts.',
+      product_type: 'NORMAL',
+      image:
+        'https://cms-webserver-statics.s3.amazonaws.com/media/products/flo11_nkX1w10.jpg',
+      image2: null,
+      image3: null,
+    },
+    {
+      id: 478,
+      name: 'Technical Manuals and Protocols',
+      category: null,
+      description:
+        'Technical Manuals and Protocols are comprehensive documents designed to provide detailed instructions and guidelines for the operation, maintenance, and troubleshooting of equipment, processes, or systems.',
+      price: '1299.99',
+      stock: 100,
+      content:
+        'Detailed Instructions, Comprehensive Content, Standardized Protocols, User-Friendly Layout, Illustrations and Visuals,Protocols provide a structured, repeatable approach to specific processes or procedures,Technical Language and Terminology, Safety and Compliance Guidelines.',
+      product_type: 'NORMAL',
+      image:
+        'https://cms-webserver-statics.s3.amazonaws.com/media/products/flo_16.jpg',
+      image2: null,
+      image3: null,
+    },
+    {
+      id: 483,
+      name: 'Consultation Packages',
+      category: null,
+      description:
+        'Consultation Packages are structured offerings provided by professionals or organizations to deliver expert advice, support, and solutions in a specific field or industry. These packages are designed to address particular needs, provide comprehensive guidance, and help clients achieve their goals through tailored consultation services.',
+      price: '599.99',
+      stock: 100,
+      content:
+        "Defined Scope of Services, Customization: Packages can be tailored to meet the unique needs of different clients, Structured Duration, Access to Expertise, Many packages include initial assessments to evaluate the client's current situation, Follow-Up Support, Resource Materials.",
+      product_type: 'NORMAL',
+      image:
+        'https://cms-webserver-statics.s3.amazonaws.com/media/products/flo15.jpg',
+      image2: null,
+      image3: null,
+    },
+    {
+      id: 480,
+      name: 'Simulation Models',
+      category: null,
+      description:
+        'Simulation Models are computational tools or mathematical models that replicate real-world processes, systems, or phenomena to analyze their behavior under different conditions,They are used to study complex systems where experimentation with the actual system is impractical, dangerous, or cost-prohibitive.',
+      price: '1499.99',
+      stock: 100,
+      content:
+        'Dynamic System Representation, Scenarios and What-If Analysis, Complexity Management, Data Integration: Simulation models often incorporate real-world data, Stochastic or Deterministic Models, Visualization: Advanced simulation software often includes graphical displays, User Interface.',
+      product_type: 'NORMAL',
+      image:
+        'https://cms-webserver-statics.s3.amazonaws.com/media/products/flo12_Y3EL3TX.jpg',
+      image2: null,
+      image3: null,
+    },
+    {
+      id: 481,
+      name: 'Scientific White Papers',
+      category: null,
+      description:
+        'Scientific White Papers are authoritative reports that provide comprehensive and detailed information about a specific topic, typically in the fields of science, technology, or academi, These documents are used to communicate research findings, share expert knowledge, propose solutions to complex problems, or advocate for a specific position within the scientific community.',
+      price: '1799.99',
+      stock: 100,
+      content:
+        'In-Depth Analysis, Evidence-Based Content: The content is supported by empirical data, research studies,Structured Format, Objective and Neutral Tone, Technical Language and Jargon, Illustrations and Visuals, References and Citations, Problem-Solving Focus, Call to Action or Recommendations.',
+      product_type: 'NORMAL',
+      image:
+        'https://cms-webserver-statics.s3.amazonaws.com/media/products/flo13.jpg',
+      image2: null,
+      image3: null,
+    },
+    {
+      id: 482,
+      name: 'Training Modules and e-Learning Courses',
+      category: null,
+      description:
+        'Training Modules and e-Learning Courses are structured educational programs designed to facilitate learning through digital platforms,They combine various multimedia elements, instructional strategies, and assessments to provide comprehensive training on a wide range of subjects.',
+      price: '1399.99',
+      stock: 100,
+      content:
+        'Training is often broken down into smaller, manageable modules or units, Interactive Content, Multimedia Utilization, Accessible Anytime, Anywhere, Assessment and Feedback, Learning Management Systems, Upon completion, learners may receive certificates or credentials that acknowledge their achievement.',
+      product_type: 'NORMAL',
+      image:
+        'https://cms-webserver-statics.s3.amazonaws.com/media/products/flo14.jpg',
+      image2: null,
+      image3: null,
+    },
+    {
+      id: 476,
+      name: 'Customized Research Reports',
+      category: null,
+      description:
+        'Customized Research Reports are tailored documents designed to provide in-depth analysis and insights that meet the specific needs and requirements of a client, These reports are highly versatile and can cover a range of industries, markets, and business topics',
+      price: '699.99',
+      stock: 100,
+      content:
+        'Tailored Content, Comprehensive Analysis, Industry-Specific Focus, Up-to-Date Information: The data and analysis provided are based on the latest available information, Expert Insights, Data Visualization: These reports commonly feature charts, graphs, Flexible Formats.',
+      product_type: 'NORMAL',
+      image:
+        'https://cms-webserver-statics.s3.amazonaws.com/media/products/flo9.jpg',
+      image2: null,
+      image3: null,
+    },
+  ],
+  categories: [],
+};
